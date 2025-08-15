c60fafdb6ff2a38349abfb3f65d5e8e44a706d81
Quang Trung Ta <taquangtrungvn@gmail.com>
2025-08-15 17:43:08 +0800

    print issues
    Signed-off-by: Quang Trung Ta <taquangtrungvn@gmail.com>

--- issue/src/issue.rs
+++ issue/src/issue.rs
@@ -2,12 +2,14 @@
 // Data structures representing smart contract issues.
 //-------------------------------------------------------------------------
 
+use std::fmt::{self, Display};
+
 pub struct Issue {
     pub name: String,
     pub description: String,
     pub severity: Severity,
-    pub cwe_id: Option<usize>, // CWE: https://cwe.mitre.org/index.html
-    pub swc_id: Option<usize>, // SWC: https://swcregistry.io/
+    pub cwe_ids: Vec<usize>, // Related CWE: https://cwe.mitre.org/index.html
+    pub swc_ids: Vec<usize>, // Related SWC: https://swcregistry.io/
 }
 
 pub enum Severity {
@@ -27,9 +29,42 @@ impl Issue {
         name: String,
         description: String,
         severity: Severity,
-        cwe_id: Option<usize>,
-        swc_id: Option<usize>,
+        cwe_ids: Vec<usize>,
+        swc_ids: Vec<usize>,
     ) -> Self {
-        Issue { name, description, severity, swc_id, cwe_id }
+        Issue { name, description, severity, swc_ids, cwe_ids }
+    }
+}
+
+impl Display for Issue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Issue: {}", self.name)?;
+        writeln!(f, "Description: {}", self.description)?;
+        writeln!(f, "Severity: {}", self.severity)?;
+        if !self.cwe_ids.is_empty() {
+            writeln!(f, "Related CWE IDs: {:?}", self.cwe_ids)?;
+        }
+        if !self.swc_ids.is_empty() {
+            writeln!(f, "Related SWC IDs: {:?}", self.swc_ids)?;
+        }
+        Ok(())
+    }
+}
+
+//-------------------------------------------------------------------------
+// Implementation for Severity
+//-------------------------------------------------------------------------
+
+impl Severity {}
+
+impl Display for Severity {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Severity::Informational => write!(f, "Informational"),
+            Severity::Low => write!(f, "Low"),
+            Severity::Medium => write!(f, "Medium"),
+            Severity::High => write!(f, "High"),
+            Severity::Critical => write!(f, "Critical"),
+        }
     }
 }
